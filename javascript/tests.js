12eb2511b94f96487c1e1e0d367e1fb7dbc9f38a
Bobenheimer <bobctoney@gmail.com>
2012-10-29 19:24:32 -0700

    Added basic logic for building tracks, added test script

---
+++ javascript/tests.js
@@ -0,0 +1,20 @@
+var toString = function(arr) {
+    var i;
+    var str = "[";
+    for (i=0; i < arr.length; i++)
+        str += arr[i] + ",";
+    console.log(str + "]");
+}
+
+trackbuilder = new TrackBuilder();
+trackbuilder.addNote(440, 0, 1);
+trackbuilder.addNote(660, 2, 1);
+trackbuilder.addNote(880, 5, 1);
+trackbuilder.addNote(550, 1, 1);
+trackbuilder.addNote(550, 3, 2);
+//poo.addNote(880, 3, 1);
+toString(trackbuilder.durationPattern);
+toString(trackbuilder.frequencyPattern);
+toString(trackbuilder.beatPattern);
+console.log(trackbuilder.lastBeat);
+trackbuilder.play();
