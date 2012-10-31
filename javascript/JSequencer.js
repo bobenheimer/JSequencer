4616b07e112f612092686b27d9c15b9c6152c776
Bobenheimer <bobctoney@gmail.com>
2012-10-30 23:42:29 -0700

    added remove track

--- javascript/JSequencer.js
+++ javascript/JSequencer.js
@@ -1,8 +1,7 @@
-
-
-var TrackBuilder = function() {
+var TrackBuilder = function(instrument) {
     this.audiolet = new Audiolet();
     this.notes = [];
+    this.instrument = instrument;
 }
 
 /**
@@ -30,8 +29,13 @@ TrackBuilder.prototype.changeTempo = function(newTempo) {
 /**
  * Remove note given
  */
-TrackBuilder.prototype.removeNote = function() {
-    
+TrackBuilder.prototype.removeNote = function(note) {   
+    //binary search tree seems kind of overkill for now
+    for (var i = 0; i < this.notes.length; i++) {
+        if (this.notes[i] == note) {
+            this.notes.splice(i, 1);
+        }
+    }
 }
 
 /**
@@ -40,12 +44,13 @@ TrackBuilder.prototype.removeNote = function() {
 TrackBuilder.prototype.play = function() {
     for (var i = 0; i < this.notes.length; i++) {
         this.audiolet.scheduler.addRelative(this.notes[i].beat, this.playNote.bind(this, this.notes[i].frequency));
+        this.audio
     }
 }
 
 TrackBuilder.prototype.playNote = function(frequency) {
-    var synth = new Synth(this.audiolet, frequency);
-    synth.connect(this.audiolet.output);  
+    var note = new this.instrument(this.audiolet, frequency);
+    note.connect(this.audiolet.output);
 }
 
 var Note = function(frequency, beat, length) {
@@ -58,7 +63,11 @@ Note.prototype.toString = function() {
     return "frequency:" + this.frequency + " beat: " + this.beat + " length: " + this.length; 
 }
 
-var Synth = function(audiolet, frequency, attack, release ) {
+
+
+
+var Synth1 = function(audiolet, frequency, attack, release ) {
+    //console.log(frequency)
     AudioletGroup.apply(this, [audiolet, 0, 1]);
     // Basic wave
     this.sine = new Sine(audiolet, frequency);
@@ -67,7 +76,7 @@ var Synth = function(audiolet, frequency, attack, release ) {
     this.gain = new Gain(audiolet);
     this.env = new PercussiveEnvelope(audiolet, 1, 0.01, .55,
         function() {
-            this.audiolet.scheduler.addRelative(0, this.remove.bind(this));
+            audiolet.scheduler.addRelative(0, this.remove.bind(this));
         }.bind(this)
     );
     this.envMulAdd = new MulAdd(audiolet, 0.2, 0);
@@ -80,5 +89,4 @@ var Synth = function(audiolet, frequency, attack, release ) {
     this.env.connect(this.envMulAdd);
     this.envMulAdd.connect(this.gain, 0, 1);
 }
-extend(Synth, AudioletGroup);
-
+extend(Synth1, AudioletGroup);
--- javascript/tests.js
+++ javascript/tests.js
@@ -1,15 +1,14 @@
-trackbuilder = new TrackBuilder();
-trackbuilder.addNote(new Note(440, 0, 1));
-trackbuilder.addNote(new Note(660, 1, 1));
-/*trackbuilder.addNote(new Note(660, 0, 8));
-trackbuilder.addNote(new Note(660, 2, 1));
-trackbuilder.addNote(new Note(880, 5, 1));
-trackbuilder.addNote(new Note(659.26, 1, 1));
-trackbuilder.addNote(new Note(659.26, 3, 2));
-trackbuilder.addNote(new Note(523.25, 6, 2));*/
-for (var i = 0; i < trackbuilder.notes.length; i++)
-{
-    console.log(trackbuilder.notes[i].toString());
-}
-//trackbuilder.changeTempo()
+var trackbuilder = new TrackBuilder(Synth1);
+var notes = [];
+notes [notes.length] = new Note(440, 0, 1); 
+notes [notes.length] = new Note(660, 1, 1);
+notes [notes.length] = new Note(550, .5, .5);
+
+
+
+for (var i = 0; i < notes.length; i++) {
+    trackbuilder.addNote(notes[i]);
+} 
+
+trackbuilder.changeTempo(140);
 trackbuilder.play();
