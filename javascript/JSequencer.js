9b278f1b6ad08cc3768245837d229f3c399abc2f
Bob Toney <bobctoney@gmail.com>
2012-11-01 11:51:04 -0700

    Added Song class

--- javascript/JSequencer.js
+++ javascript/JSequencer.js
@@ -34,47 +34,84 @@ TrackBuilder.prototype.removeNote = function(note) {
     for (var i = 0; i < this.notes.length; i++) {
         if (this.notes[i] == note) {
             this.notes.splice(i, 1);
+            return;
         }
     }
 }
 
 /**
  * Play the song
+ * may want to add startbeat as an instance variable or something and then have a setter function
  */
-TrackBuilder.prototype.play = function() {
-    for (var i = 0; i < this.notes.length; i++) {
-        this.audiolet.scheduler.addRelative(this.notes[i].beat, this.playNote.bind(this, this.notes[i].frequency));
-        this.audio
-    }
+TrackBuilder.prototype.play = function(beat) {
+	var difference = 0, start = 0;
+	if(beat > this.notes[this.notes.length - 1].beat) {
+		start = this.notes.length;
+	}
+	else if(beat) {
+		var i = 0;
+		while(beat > this.notes[i].beat) {
+			i++;
+			//console.log(i);
+		}
+		//console.log(i);
+		start = i;
+		difference = this.notes[i].beat - beat;
+		console.log(difference);
+	}
+	for (var i = start; i < this.notes.length; i++) {
+	    this.audiolet.scheduler.addRelative(difference + 
+	    		this.notes[i].beat, this.playNote.bind(this, this.notes[i]));
+	}
 }
 
-TrackBuilder.prototype.playNote = function(frequency) {
-    var note = new this.instrument(this.audiolet, frequency);
+TrackBuilder.prototype.playNote = function(note) {
+    var note = new this.instrument(this.audiolet, note.frequency, note.duration);
     note.connect(this.audiolet.output);
 }
 
-var Note = function(frequency, beat, length) {
+/**
+ * Get the index where the Beat should be
+ */
+TrackBuilder.prototype.getBeatIndex = function(beat) {
+	for (var i = 0; i < this.notes.length; i++) {
+		
+	}
+}
+
+var Note = function(frequency, beat, duration) {
     this.frequency = frequency;
     this.beat = beat;
-    this.length = length;    
+    this.duration = duration;    
 }
 
 Note.prototype.toString = function() {
-    return "frequency:" + this.frequency + " beat: " + this.beat + " length: " + this.length; 
+    return "frequency:" + this.frequency + " beat: " + this.beat + " duration: " + this.duration; 
+}
+
+var Song = function() {
+	this.tempo = 100;
+	this.tracks = [];
+}
+
+Song.prototype.play = function() {
+	for (var i = 0; i < this.tracks.length; i++) {
+		//add i guess
+	}
 }
 
 
-
-
-var Synth1 = function(audiolet, frequency, attack, release ) {
-    //console.log(frequency)
+var Synth1 = function(audiolet, frequency, duration) {
     AudioletGroup.apply(this, [audiolet, 0, 1]);
     // Basic wave
+    var attack = 0.01;
+    var release = 0.5 * duration;
+    console.log(release);
     this.sine = new Sine(audiolet, frequency);
     
     // Gain envelope
     this.gain = new Gain(audiolet);
-    this.env = new PercussiveEnvelope(audiolet, 1, 0.01, .55,
+    this.env = new PercussiveEnvelope(audiolet, 1, 0.01, release,
         function() {
             audiolet.scheduler.addRelative(0, this.remove.bind(this));
         }.bind(this)
--- javascript/tests.js
+++ javascript/tests.js
@@ -1,14 +1,28 @@
 var trackbuilder = new TrackBuilder(Synth1);
 var notes = [];
-notes [notes.length] = new Note(440, 0, 1); 
-notes [notes.length] = new Note(660, 1, 1);
-notes [notes.length] = new Note(550, .5, .5);
+/*notes [notes.length] = new Note(880, 0, 1); 
+notes [notes.length] = new Note(1320, 1, 1);
+notes [notes.length] = new Note(880, 2, 1); 
+notes [notes.length] = new Note(1320, 3, 1);
+notes [notes.length] = new Note(880, 4, 1); 
+notes [notes.length] = new Note(1320, 5, 2);
 
 
+notes [notes.length] = new Note(440, 0, 0.5); 
+notes [notes.length] = new Note(523.25, 0.5, 0.5);
+notes [notes.length] = new Note(659.26, 1, 0.5);
+notes [notes.length] = new Note(880, 1.5, 1);
+notes [notes.length] = new Note(659.26, 2.5, 0.5);
+notes [notes.length] = new Note(523.25, 3, 0.5);
+notes [notes.length] = new Note(440, 3.5, 2);*/
+
+notes [notes.length] = new Note(880, 0, 1); 
+notes [notes.length] = new Note(1320, 1, 2);
 
 for (var i = 0; i < notes.length; i++) {
     trackbuilder.addNote(notes[i]);
 } 
+
 
 trackbuilder.changeTempo(140);
 trackbuilder.play();
